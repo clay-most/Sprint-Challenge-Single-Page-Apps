7247a995659e3a3d70862930c0ffd886dbb1e864
clay-most <46730958+clay-most@users.noreply.github.com>
2019-09-15 18:48:13 -0600

    character card/list

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+  <div className="characterCard">
+    <h4>{props.name}</h4>
+    <img src={props.img}></img>
+    <p>Species:{props.species}Origin:{props.origin}Status:{props.status}Location:{props.location}</p>
+  </div>
+    );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+      .then(response => {
+        const chars = response.data;
+        setCharacters(chars.results);
+      })
+      .catch(error => {
+        console.log('error', error);
+      });
   }, []);
 
   return (
     <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map(character => {
+        return (
+          <CharacterCard
+          key={character.id}
+          name={character.name}
+          image={character.image}
+          location={character.location}
+          origin={character.origin}
+          species={character.species}
+          status={character.status}
+          >
+          </CharacterCard>
+        );
+      })}
     </section>
   );
 }
