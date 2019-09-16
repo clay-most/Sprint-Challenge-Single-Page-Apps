fda2930632de1844b4bd83762129b9accb700a4e
clay-most <46730958+clay-most@users.noreply.github.com>
2019-09-15 19:15:02 -0600

    location card/list

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React from 'react';
 
 export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+  return (
+    <div>
+      <h4>{name}</h4>
+      <p>
+        Type:{type} Dimension:{dimension}
+      </p>
+      <ul>
+        Residents
+        {residents.map(resident => {
+          return <li>{resident}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,36 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios"
+import LocationCard from "./LocationCard"
 
-export default function LocationsList() {}
+export default function LocationsList() {
+  const [locations, setLocations] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+      .then(response => {
+        const locs = response.data;
+        setLocations(locs.results);
+      })
+      .catch(error => {
+        console.log('error', error);
+      });
+  }, []);
+
+  return (
+    <section className="location-list">
+      {locations.map(location => {
+        return (
+          <LocationCard
+          key={location.id}
+          name={location.name}
+          type={location.type}
+          dimension={location.dimension}
+          residents={location.residents}
+          >
+          </LocationCard>
+        );
+      })}
+    </section>
+  );
+}
