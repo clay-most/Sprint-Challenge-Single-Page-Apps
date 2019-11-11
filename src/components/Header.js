75f533cb7d9c3feec4ac9baeaf20724751e6aec7
Anna Morris <annapatton444@gmail.com>
2019-11-10 19:35:15 -0600

    serch form too complicated for time limit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
@@ -8,6 +8,7 @@ export default function Header() {
       <nav>
         <Link to="/">Home</Link>
         <Link to="/characters">Characters</Link>
+        <Link to="/search">Search</Link>
       </nav>
     </header>
   );
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import CharacterCard from "./CharacterCard";
+import Axios from "axios";
 
 export default function SearchForm() {
- 
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    Axios.get("https://rickandmortyapi.com/api/character/").then(response => {
+      const resultsLocal = response.data.results.filter(character =>
+        character.name.toLowerCase().includes(search.toLocaleLowerCase())
+      );
+      setData(resultsLocal);
+    });
+  }),
+    [search];
+
+  const HandleChange = event => {
+    setSearch(event.target.value);
+  };
+
   return (
     <section className="search-form">
-     // Add a search form here
+      <form>
+        <input type="text" name="search" placeholder="Search by Name" />
+        <button>Search</button>
+      </form>
+      {data.map(character => {
+        return (
+          <CharacterCard
+            key={character.id}
+            name={character.name}
+            type={character.type}
+            gender={character.gender}
+            img={character.image}
+          ></CharacterCard>
+        );
+      })}
     </section>
   );
 }
